75d3332396f7f4ab7d4b7ae380d1706acb7a6fbc
Victor Aurelio <victoraurelio_198@hotmail.com>
2023-03-28 15:18:51 -0300

    Fixing cors issues

--- App/Core/Config.php
+++ App/Core/Config.php
@@ -41,14 +41,10 @@ class Config
     public function configureCors()
     {
         if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-                $allowedOrigins = array('http://localhost:3000', 'http://example.com');
-                $origin = $_SERVER['HTTP_ORIGIN'];
-                if (in_array($origin, $allowedOrigins)) {
-                    header('Access-Control-Allow-Origin: ' . $origin);
-                    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-                    header('Access-Control-Allow-Headers: Content-Type, Authorization');
-                    header('Access-Control-Allow-Credentials: true');
-                }
+            header('Access-Control-Allow-Origin: *');
+            header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+            header('Access-Control-Allow-Headers: Origin,X-Requested-With,Content-Type,Accept,content-type,application/json, authorization');
+            header('Access-Control-Allow-Credentials: true');
             exit;
         }
     }
