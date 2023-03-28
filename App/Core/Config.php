09538547ddc51c1a4eb3cff7d8810fdf39a59759
Victor Aurelio <victoraurelio_198@hotmail.com>
2023-03-28 14:25:38 -0300

    Changing cors settings

--- App/Core/Config.php
+++ App/Core/Config.php
@@ -49,6 +49,6 @@ class Config
     {
         header('Access-Control-Allow-Headers: Content-Type, Authorization');
         header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-        header('Access-Control-Allow-Origin: http://localhost:3000/');
+        header('Access-Control-Allow-Origin: *');
     }
 }
