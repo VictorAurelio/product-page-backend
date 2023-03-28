eb32360407a750a140c6f6157d7fd2aab8a1cccd
Victor Aurelio <victoraurelio_198@hotmail.com>
2023-03-28 15:26:54 -0300

    Fixing cors issues

--- App/Core/Config.php
+++ App/Core/Config.php
@@ -42,12 +42,13 @@ class Config
     {
         if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
             $this->setCorsHeaders();
+            exit;
         }
     }
     private function setCorsHeaders()
     {
         header('Access-Control-Allow-Headers: Content-Type, Authorization');
-        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, PATCH');
         header('Access-Control-Allow-Origin: *');
     }
 }
